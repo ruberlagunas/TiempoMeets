34956c4b554654a752f7254d0aa093204f89395a
Ruber Lagunas <rlagunas@tiempodevelopment.com>
2017-11-08 12:18:32 -0600

    Initial Commit

---
+++ DetectObjects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetectObjects
+//
+//  Created by Ruber Magdiel Lagunas Arango on 11/8/17.
+//  Copyright © 2017 Ruber Magdiel Lagunas Arango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
